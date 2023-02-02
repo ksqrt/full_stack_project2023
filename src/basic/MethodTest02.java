ef28d9f7618a944fd953498fe786ed35b7dd162f
ksqrt <ksqrt98@gmail.com>
2023-02-02 03:03:28 +0000

    랜덤 함수 추가

--- src/basic/MethodTest02.java
+++ src/basic/MethodTest02.java
@@ -1,5 +1,15 @@
 package src.basic;
 
+import java.util.Random;;
+
 public class MethodTest02 {
-    
+    public static void main(String[] args) {
+ 
+        // 난수 = 랜덤
+        
+        Random r = new Random();
+        double b = r.nextDouble();
+
+        System.out.println("난수 = " +b);
+    }   
 }
