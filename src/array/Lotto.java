776ea6b7d1e04239b4f34aa39607677374874042
ksqrt <ksqrt98@gmail.com>
2023-02-07 05:31:50 +0000

    중복 제거 로또

---
+++ src/array/Lotto.java
@@ -0,0 +1,59 @@
+package src.array;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
+
+// [문제] 로또 - 자동
+// - 크기가 6개인 배열 생성
+// - 1 ~ 45 사이의 난수 발생
+// - 숫자는 오름차순하여 출력 (Selection Sort)
+// - 출력시 자리수는 5자리로 지정
+// - 중복 숫자가 나오면 안된다
+// - 1000원당 1줄이 나온다 
+// - 5줄마다 줄바꿈
+
+// [실행결과]
+// 현금 입력 : 7000
+//     2    4   19   39   43   44
+//    22   26   33   38   39   42
+//     5    6    8   25   35   45
+//     3   14   23   30   34   35
+//    18   20   25   27   32   37
+   
+//     1   16   32   34   41   42
+//     5    6   18   30   33   44
+
+
+public class Lotto {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("현금 입력 : ");
+        int money = scan.nextInt();
+
+        for(int i =0; i<(money/1000)+1; i++){
+            Integer[] lotto = new Integer[6];
+            for(int j =0; j<lotto.length; j++){
+                lotto[j] = (int)((Math.random()*45));
+            }
+
+            Set<Integer> set = new HashSet<Integer>(Arrays.asList(lotto));
+            Integer[] arr = set.toArray(new Integer[0]);
+            if(arr.length == 6){
+                for(int j =0; j<lotto.length; j++){
+                    System.out.printf("%5d",lotto[j]);
+                }
+                System.out.println("");
+                if(i!= 0 && i%5 == 0){
+                    System.out.println();
+                }
+            }
+            else{
+                i--;
+            }
+
+        }
+        
+
+    }   
+}
--- src/array/SelectionSort.java
+++ src/array/SelectionSort.java
@@ -2,24 +2,29 @@ package src.array;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] ar = {25,40,32,78,56};
         // for(int i=0; i<ar.length; i++){
         //     System.out.println(ar[i]+"\t");    
         // }   
         // System.out.println();
+        int[] lotto = new int[6];
+        for(int i =0; i<lotto.length; i++){
+            lotto[i] = (int)(Math.random()*45 +1);
+        }
+
+
         int temp;
-        for(int i=0; i <ar.length-1 ;i++){
-            for(int j = i+1 ; j < ar.length ; j++){
-                System.out.println("i : "+i+" j : "+j);
-                if(ar[i]<ar[j]){
-                    temp = ar[i];
-                    ar[i] = ar[j];
-                    ar[j] = temp;}
+        for(int i=0; i <lotto.length-1 ;i++){
+            for(int j = i+1 ; j < lotto.length ; j++){
+                // System.out.println("i : "+i+" j : "+j +" lotto :"+lotto[java.awt]);
+                if(lotto[i]>lotto[j]){
+                    temp = lotto[i];
+                    lotto[i] = lotto[j];
+                    lotto[j] = temp;}
         
             }
         }
-        for(int i=0; i<ar.length; i++){
-        System.out.println(ar[i]+"\t");    
+        for(int i=0; i<lotto.length; i++){
+        System.out.println(lotto[i]+"\t");    
         }
    }
 }
