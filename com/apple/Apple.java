79f539074da2054feea3551d78a734a704e274d9
ksqrt <ksqrt98@gmail.com>
2023-02-14 01:41:18 +0000

    접근

---
+++ com/apple/Apple.java
@@ -0,0 +1,12 @@
+package com.apple;
+
+import com.zoo.Zoo;
+
+public class Apple {
+    public static void main(String[] args) {
+        System.out.println("사과");
+        // zoo 클래스의 tiger 호출
+        Zoo zoo = new Zoo();
+        zoo.tiger();
+    }
+}
