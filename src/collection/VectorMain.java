9550e4d827c65e6dee11bc4ed126251ab27c4dec
ksqrt <ksqrt98@gmail.com>
2023-02-16 05:30:52 +0000

    xml

--- src/collection/VectorMain.java
+++ src/collection/VectorMain.java
@@ -1,7 +1,45 @@
 package src.collection;
 
+import java.util.Iterator;
+import java.util.Vector;
+
 public class VectorMain {
     public static void main(String[] args) {
+        Vector<String> v = new Vector<String>();
+        System.out.println("벡터 크기 = " + v.size());
+        System.out.println("벡터 용량 = " + v.capacity());
+        System.out.println();
 
+        System.out.println("항목 추가");
+        for (int i = 0; i < 10; i++) {
+            v.add((i + 1) + "");
+            System.out.print(v.get(i) + " ");
+        }
+        System.out.println();
+        System.out.println("벡터 크기 = " + v.size());
+        System.out.println("벡터 용량 = " + v.capacity());
+        System.out.println();
+
+        System.out.println("항목 1개 추가");
+        v.addElement(5 + "");
+        System.out.println();
+        System.out.println("벡터 크기 = " + v.size());
+        System.out.println("벡터 용량 = " + v.capacity());
+        System.out.println();
+
+        for (int i = 0; i < v.size(); i++) {
+            System.out.print(v.get(i) + " ");
+        }
+
+        System.out.println();
+        System.out.println("마지막항목 1개 삭제");
+        v.remove(10);
+
+        // v 를 이터레이터로
+        Iterator<String> it = v.iterator();
+        while (it.hasNext()) {
+            System.out.print(it.next() + " ");
+        }
+        System.out.println();
     }
 }
--- sungjuk/SungJukDelete.java
+++ sungjuk/SungJukDelete.java
@@ -1,6 +1,7 @@
 package sungjuk;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class SungJukDelete implements SungJuk {
@@ -14,24 +15,29 @@ public class SungJukDelete implements SungJuk {
         String name1 = scan.nextLine();
         int check = 0;
 
-        for (int i = 0; i < arrayList.size(); i++) {
-            // 입력한 이름이 있으면 삭제하고 채크카운트 +1
-            if (arrayList.get(i).getName().equals(name1)) {
-                arrayList.remove(i);
+//        for (int i = 0; i < arrayList.size(); i++) {
+//            // 입력한 이름이 있으면 삭제하고 채크카운트 +1
+//            if (arrayList.get(i).getName().equals(name1)) {
+//                arrayList.remove(i);
+//                check = check + 1;
+//            }
+//        }
+//
+        // 1. 배열을 이터레이터로 변환합니다.
+        Iterator<SungJukDTO> it = arrayList.iterator();
+        while (it.hasNext()) {
+            SungJukDTO sungJukDTO = it.next();
+            if (sungJukDTO.getName().equals(name1)) {
+                it.remove();
                 check = check + 1;
+
             }
         }
-        // for (SungJukDTO sungJukDTO : arrayList) {
-        // // 입력한 이름이 있으면 삭제하고 채크카운트 +1
-        // if (sungJukDTO.getName().equals(name1)) {
-        // arrayList.remove(sungJukDTO);
-        // check = check + 1;
-        // }
-        // }
+
         if (check == 0) {
             System.out.println("회원의 정보는 없습니다.");
         } else {
-            System.out.println("삭제 하였습니다.");
+            System.out.println(check + " 건을 삭제 하였습니다.");
         }
     }
 
