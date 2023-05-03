a6e34656a7850e3a0da548447ad719d25c47f5b0
ksqrt <ksqrt98@gmail.com>
2023-05-03 16:30:51 +0900

    리액트 설치

---
+++ tecmint-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Dog from './Dog.js'
+import Test1 from './test1.js'
+
+const App = () =>{
+  return (
+ 
+    <div>
+      <Dog></Dog>
+      <Dog></Dog>
+      <Test1></Test1>
+
+    </div>
+  )
+}
+
+
+
+export default App;
---
+++ tecmint-app/src/Dog.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dog= () =>{
+    return(
+
+        <div>
+            <h3>Dog 컴포넌트</h3>
+        </div>
+    )
+}
+
+export default Dog;
