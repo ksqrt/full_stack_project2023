50bf5f4be8ccaebcba96646c0b97d998bc0a7d3f
ksqrt <ksqrt98@gmail.com>
2023-05-03 16:36:40 +0900

    test2 수정

--- tecmint-app/src/App.js
+++ tecmint-app/src/App.js
@@ -2,15 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Dog from './Dog.js'
 import Test1 from './test1.js'
+import Test2 from './test2.js'
 
 const App = () =>{
+  
+  let title = "신상명세서"
+  const name = "홍길동"
+
   return (
  
     <div>
       <Dog></Dog>
       <Dog></Dog>
       <Test1></Test1>
-
+      <Test2></Test2>
     </div>
   )
 }
