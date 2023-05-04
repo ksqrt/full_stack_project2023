bd5e4eaecc7a28228675dd8ede1c2c177c1d0080
ksqrt <ksqrt98@gmail.com>
2023-05-04 17:28:18 +0900

    반복문으로 출력해보기

--- day02/src/App.js
+++ day02/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Test01 from './component/Test01'
+import Test02 from './component/Test02'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       {/* <Test01></Test01> */}
+       <Test02></Test02>
     </div>
   );
 }
