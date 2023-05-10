6d2c304a6154c1a2a0917d3ce2358494c8702092
ksqrt <ksqrt98@gmail.com>
2023-05-10 17:52:53 +0900

    ㅇㅇ

--- day04/src/App.js
+++ day04/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Test01 from './component/Test01';
+import Test02 from './component/Test02';
 
 const App = () => {
   return (
     <div>
-      <Test01></Test01>
+      {/* <Test01></Test01> */}
+      <Test02></Test02>
     </div>
   );
 };
