962b269ced2f1ec0d4faae240a995e622d70d83c
ksqrt <ksqrt98@gmail.com>
2023-05-11 16:13:35 +0900

    state 로 검색기능구현하기

--- day05/src/component/Test01.js
+++ day05/src/component/Test01.js
@@ -2,15 +2,41 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Test01 = () => {
-    const [data,setData] = useState([
+    const [data,setData] = useState([])
+    // 1 번 
+    // useEffect(()=>{
+    //     fetch('https://jsonplaceholder.typicode.com/posts')
+    //     .then(res => res.json() )
+    //     .then(res => setData(res))
+    // },[])
+    
+    // 2 번
+    // useEffect(()=>{
+    //     axios.get('https://jsonplaceholder.typicode.com/posts')
+    //     .then(res => setData(res.data))
+    // },[])
 
-        // 1 번 
-        useEffect(()=>{
-            fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(res => res.json() )
-            .then(res => setData(res))
-        },[])
-    ])
+    // 3번
+    // useEffect(()=>{
+    //     const getData = async()=>{
+    //         const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    //         const data = await res.json()
+    //         setData(data)
+    //     }
+    //     getData()
+    // },[])
+
+    // 4번 
+    useEffect(()=>{
+        const getData = async()=>{
+            const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+            setData(res.data)
+        }
+        getData()
+    },[])
+
+    console.log(data)
+
     return (
         <div>
            {
