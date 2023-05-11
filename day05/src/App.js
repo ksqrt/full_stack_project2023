e3c6962e4df5f5a441c802bd056b56691234f6f7
ksqrt <ksqrt98@gmail.com>
2023-05-11 17:29:08 +0900

    usestate 사용하기

--- day05/src/App.js
+++ day05/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Test01 from './component/Test01';
 import Test02 from './component/Test02';
+import Test03 from './component/Test03';
+import Test04 from './component/Test04';
 
 const App = () => {
   return (
     <div>
       {/* <Test01></Test01>       */}
-      <Test02></Test02>
+      {/* <Test02></Test02> */}
+      {/* <Test03></Test03> */}
+      <Test04></Test04>
     </div>
   );
 };
